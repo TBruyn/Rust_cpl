55906b696cede92efadfac82adb6f520d70f0ac8
TBruyn <timtbruyn@gmail.com>
2018-10-10 21:43:09 +0200

    Implement parse_s_expr stable

--- src/interp_basic.rs
+++ src/interp_basic.rs
@@ -2,23 +2,64 @@ use expression::*;
 use expression::ExprExt::*;
 use expression::SExpr::*;
 
-pub fn parse_SExpr(expression: &SExpr) -> ExprExt {
+pub fn parse_s_expr(expression: &SExpr) -> ExprExt {
     match expression {
         SList(vec) => 
             match vec[..] {
                 [ref s, ref l,  ref r] => 
                     match s {
                         SSym(sym) => return BinOpExt( sym.to_string(), 
-                                                Box::new(parse_SExpr(&l)), 
-                                                Box::new(parse_SExpr(&r))   ),
-                        _ => panic!("Unexpected expression")
+                                                Box::new(parse_s_expr(&l)), 
+                                                Box::new(parse_s_expr(&r))   ),
+                        _ => panic!("Unexpected expression: {:?}", s)
                     },
-
-            
-            
-                           _ => panic!("Unexpected expression")
-        }
-        _ => panic!("Unexpected expression")
-        // SList([SSym(s), a, b]) => BinOpExt(s, parse_SExpr(a), parse_SExpr(b))
+                [ref s, ref e] => 
+                    match s {
+                        SSym(sym) => return UnOpExt( sym.to_string(), 
+                                                Box::new(parse_s_expr(&e))  ),
+                        _ => panic!("Unexpected expression: {:?}", s)
+                    },
+                [ref s, ref c, ref l, ref r] => 
+                    match s {
+                        SSym(sym) if sym == "if" => return IfExt( 
+                                                Box::new(parse_s_expr(&c)),
+                                                Box::new(parse_s_expr(&l)),
+                                                Box::new(parse_s_expr(&r))  ),
+                        _ => panic!("Unexpected expression: {:?}", s)
+                    },
+                _ => {
+                        match vec[0] {
+                            SSym(ref s) => match s.as_ref() {
+                                "tuple" => {
+                                    let mut items: Vec<ExprExt> = Vec::new();
+                                    for ref item in &vec[1..] {
+                                        items.push(parse_s_expr(&item));
+                                    }
+                                    return TupleExt(items);
+                                },
+                                _ => panic!("Unexpected expression: {:?}", vec)
+                            },
+                            _ => panic!("Unexpected expression: {:?}", vec)
+                        }
+                    }
+            },
+        SSym(ref s) =>
+            match s.as_ref() {
+                "true" => TrueExt(),
+                "false"=> FalseExt(),
+                _ => panic!("Unexpected expression")
+            },
+        SNum(ref n) => NumExt(*n)
     }
 }
+
+// #[derive(Debug)]
+// pub enum ExprExt {
+//     TrueExt(),
+//     FalseExt(),
+//     NumExt(u32),
+//     BinOpExt(   String, Box<ExprExt>, Box<ExprExt>),
+//     UnOpExt(    String, Box<ExprExt>),
+//     IfExt(Box<ExprExt>, Box<ExprExt>, Box<ExprExt>),
+//     TupleExt(Vec<ExprExt>)
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-#![feature(slice_patterns)]
-
 mod expression;
 mod tokenizer;
 mod interp_basic;
 
 fn main() {
-    let test_input = String::from("(+ 1 (- (- 1)  3))");
-    let _tokens = tokenizer::string_to_tokens(&test_input);
-    println!("{:?}", tokenizer::parse_string(&test_input));;
+    let test_input_1 = String::from("(+ 1 (- (- 1)  3))");
+    let test_input_2  = String::from("(if true 1 (+ 1 (- 2)))");
+
+    let expr_ext_1 = interp_basic::parse_s_expr(&tokenizer::parse_string(&test_input_1));
+    let expr_ext_2 = interp_basic::parse_s_expr(&tokenizer::parse_string(&test_input_2));
+
+    println!("1. {:?}", expr_ext_1);
+    println!("2. {:?}", expr_ext_2);
+
 }
